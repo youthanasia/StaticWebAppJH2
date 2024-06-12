dae96e97a1dd25ee2013e698ce687e529ed9f6ce
John Hoy <john.hoy@advania.co.uk>
2024-06-12 15:45:30 +0100

    jsupdate

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-window.FUNCTION_URL = 'https://172.211.90.143/LogAccessFunction';
+window.FUNCTION_URL = 'https://jh-staticwebapp-gateway.westeurope.cloudapp.azure.com/LogAccessFunction';
 window.FUNCTION_KEY = 'RMXMAB3peLeHcfXSDVpZq_p4yzm-SEDRG6Sfpu2z1uzsAzFu69xY0w%3D%3D'; // This should be the function key for your Azure Function
